c96e5266a34d71cdaf78e89be6e8da6fe17e1ede
sahsazjukov <sashazjukov@gmail.com>
2023-03-13 15:05:38 +0300

    dpi

--- PBSCAnalyzer/Program.cs
+++ PBSCAnalyzer/Program.cs
@@ -12,6 +12,10 @@ namespace PBSCAnalyzer
         [STAThread]
         static void Main()
         {
+            // ***this line is added***
+            if (Environment.OSVersion.Version.Major >= 6)
+                SetProcessDPIAware();
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             try
@@ -23,5 +27,9 @@ namespace PBSCAnalyzer
             }
             
         }
+
+        // ***also dllimport of that function***
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern bool SetProcessDPIAware();
     }
 }
